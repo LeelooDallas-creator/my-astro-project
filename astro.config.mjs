a9d8d838dac85c7579f52bee3070137247a568d0
Liloo Dallas <barbeylise@gmail.com>
2025-01-08 18:03:28 +0100

    ajout de themes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://boisterous-marshmallow-3fdf60.netlify.app"
+  site: "https://boisterous-marshmallow-3fdf60.netlify.app",
+  integrations: [preact()]
 });
